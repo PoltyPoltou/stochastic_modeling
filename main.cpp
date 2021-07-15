8e1454894789a44d9b3121401aac108ccd8f51a1
NP-Polty <polt.gun@gmail.com>
2021-07-15 10:56:20 +0200

    test csv

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
-#include <iostream>
 #include "coin/OsiClpSolverInterface.hpp"
-int main(int argc, char const *argv[])
-{
+#include "csv.h"
+#include "dir_separator.h"
+
+#include <iostream>
+void testCsv(std::string arg1) {
+    int last_dir_sep(arg1.find_last_of(DIR_SEP));
+    std::string dir_exec(arg1.substr(0, last_dir_sep));
+    csv::CsvFileStream csv(dir_exec + DIR_SEP + ".." + DIR_SEP + "data"
+                           + DIR_SEP + "test.csv");
+    csv.open();
+    csv::skip_line(csv);
+    std::vector<double> vector(csv::get_line(csv));
+    assert(vector[0] == 10);
+    assert(vector[1] == 10);
+    assert(vector[2] == 8);
+}
+
+int main(int argc, char const *argv[]) {
     OsiSolverInterface *test = new OsiClpSolverInterface;
     delete test;
-    std::cout << "azd" << std::endl;
+    testCsv(std::string(argv[0]));
     return 0;
 }
