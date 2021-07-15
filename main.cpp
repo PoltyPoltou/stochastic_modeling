029905443b49862796466cd5607b0bc6eb783848
NP-Polty <polt.gun@gmail.com>
2021-07-15 11:01:08 +0200

    rewrite test dans data_dir declaration

--- main.cpp
+++ main.cpp
@@ -3,22 +3,25 @@
 #include "dir_separator.h"
 
 #include <iostream>
-void testCsv(std::string arg1) {
-    int last_dir_sep(arg1.find_last_of(DIR_SEP));
-    std::string dir_exec(arg1.substr(0, last_dir_sep));
-    csv::CsvFileStream csv(dir_exec + DIR_SEP + ".." + DIR_SEP + "data"
-                           + DIR_SEP + "test.csv");
-    csv.open();
-    csv::skip_line(csv);
-    std::vector<double> vector(csv::get_line(csv));
+void testCsv(std::string data_dir) {
+    csv::CsvFileStream csv_file(data_dir + "test.csv");
+    csv_file.open();
+    csv::skip_line(csv_file);
+    std::vector<double> vector(csv::get_line(csv_file));
     assert(vector[0] == 10);
     assert(vector[1] == 10);
     assert(vector[2] == 8);
 }
 
 int main(int argc, char const *argv[]) {
+    std::string arg1(argv[0]);
+    int last_dir_sep(arg1.find_last_of(DIR_SEP));
+    std::string data_dir(arg1.substr(0, last_dir_sep) + DIR_SEP + ".." + DIR_SEP
+                         + "data" + DIR_SEP);
+
     OsiSolverInterface *test = new OsiClpSolverInterface;
     delete test;
-    testCsv(std::string(argv[0]));
+
+    testCsv(std::string(data_dir));
     return 0;
 }
