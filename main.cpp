1a542775bb442d80b00dbee729024c35833dfaa2
NP-desktop <polt.gun@gmail.com>
2021-07-16 12:16:45 +0200

    test osiCplex lib

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "coin/CoinPackedMatrix.hpp"
 #include "coin/CoinPackedVector.hpp"
 #include "coin/OsiCpxSolverInterface.hpp"
 #include "csv.h"
@@ -24,15 +25,31 @@ CoinPackedVector std_to_coin_vector(std::vector<double> &vec) {
     return coin_vec;
 }
 
+void testCplex() {
+    OsiSolverInterface *problem = new OsiCpxSolverInterface;
+
+    std::vector<double> obj({-1, -1}), col_lb({0, 0}), col_ub({100, 100}),
+        row_lb({0, 0}), row_ub({3, 3}), row1({1, 2}), row2({2, 1});
+    CoinPackedMatrix matrice;
+    matrice.setDimensions(0, 2);
+    matrice.appendRow(std_to_coin_vector(row1));
+    matrice.appendRow(std_to_coin_vector(row2));
+    problem->loadProblem(matrice, col_lb.data(), col_ub.data(), obj.data(),
+                         row_lb.data(), row_ub.data());
+    problem->initialSolve();
+    assert(problem->getObjValue() == -2);
+    assert(problem->getColSolution()[0] == 1);
+    assert(problem->getColSolution()[1] == 1);
+    delete problem;
+}
+
 int main(int argc, char const *argv[]) {
     std::string arg1(argv[0]);
     int last_dir_sep(arg1.find_last_of(DIR_SEP));
     std::string data_dir(arg1.substr(0, last_dir_sep) + DIR_SEP + ".." + DIR_SEP
                          + "data" + DIR_SEP);
 
-    OsiSolverInterface *test = new OsiClpSolverInterface;
-    delete test;
-
+    testCplex();
     testCsv(std::string(data_dir));
     return 0;
 }
