278a3b56dd7bb1480ac8f9a3fee3cbf811d75ccc
NP-Polty <polt.gun@gmail.com>
2021-07-20 10:16:20 +0200

    fix articles_max usage (now is inclusive bound)

--- probleme.cpp
+++ probleme.cpp
@@ -8,7 +8,7 @@ Probleme::Probleme(int n_cmd, double ratio_volumineux, Livraison car) :
     ratio_volu(ratio_volumineux),
     livraison_car(car) {
     if (Probleme::commandes_set.empty()) {
-        for (int n = 1; n < Probleme::articles_max; n++) {
+        for (int n = 1; n <= Probleme::articles_max; n++) {
             for (int h = 0; h < 24; h++) {
                 for (int d = Probleme::delai_min; d <= Probleme::delai_max;
                      d++) {
