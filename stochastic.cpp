38804260fd7917134c68558f721eab51007cff02
NP-Polty <polt.gun@gmail.com>
2021-07-28 11:30:40 +0200

    change in stochastic

--- stochastic.cpp
+++ stochastic.cpp
@@ -10,16 +10,16 @@ lp::LpDecatScenarios stochastic_problem(std::string data_dir,
                                         OsiSolverInterface &solver_interface,
                                         ProblemeStochastique &pb_loaded) {
     lp::LpDecatScenarios main_lp(solver_interface);
-    std::vector<double> scenarios({0.8, 0.9, 1, 1.1, 1.2});
+    std::vector<double> scenarios({0.9, 1, 1.1});
     double width = 0.05;
     main_lp.create_stock_variables(
         pb_loaded.getc_nb_articles()
         * 0.1); // coût par 100% de distribution de stock
     main_lp.create_recours_var(
-        3); // coût par article pour étendre le stock (backlog)
+        50); // coût par article pour étendre le stock (backlog)
     for (double ratio : scenarios) {
         pb_loaded.set_nb_cmd_mesured(ratio * pb_loaded.get_nb_cmd());
-        main_lp.create_variables(pb_loaded, normal_probabilty(ratio, 1, 0.3));
+        main_lp.create_variables(pb_loaded, 1 / scenarios.size());
         main_lp.fullfilment_constraint(pb_loaded);
         main_lp.stock_constraint(pb_loaded);
     }
